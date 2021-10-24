3827082b7a0f18701899a1025902c5a6103aa908
grusbarsky <gretarusbarsky@gmail.com>
2021-10-24 12:20:54 -0700

    first commit

---
+++ routes/users.js
@@ -0,0 +1,87 @@
+
+const Router = require("express").Router;
+const User = require("../models/user");
+const {checkLoggedIn, checkCorrectUser} = require("../middleware/auth");
+
+const router = new Router();
+
+
+/** get list of users.
+ *
+ * => {users: [{username, first_name, last_name, phone}, ...]}
+ *
+ **/
+
+router.get("/", checkLoggedIn, async function (req, res, next) {
+  try {
+    let users = await User.all();
+    return res.json({users});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+/** get detail of users.
+ *
+ * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
+ *
+ **/
+
+router.get("/:username", checkCorrectUser, async function (req, res, next) {
+  try {
+    let user = await User.get(req.params.username);
+    return res.json({user});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+/** get messages to user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 from_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+router.get("/:username/to", checkCorrectUser, async function (req, res, next) {
+  try {
+    let messages = await User.messagesTo(req.params.username);
+    return res.json({messages});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+/** get messages from user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 to_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+router.get("/:username/from", checkCorrectUser, async function (req, res, next) {
+  try {
+    let messages = await User.messagesFrom(req.params.username);
+    return res.json({messages});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+
+
+module.exports = router;
